448c19682186fcc8350923654ab35dd7c3a4b0cb
Maxim Kirshin <meowto16@gmail.com>
2021-04-10 23:59:33 +0500

    lesson 6.5: Authorization Updating a User's Task Status

--- projects/nestjs-task-management/src/tasks/tasks.controller.ts
+++ projects/nestjs-task-management/src/tasks/tasks.controller.ts
@@ -54,11 +54,12 @@ export class TasksController {
     return this.tasksService.deleteTaskById(id)
   }
 
-  // @Patch('/:id/status')
-  // updateTaskStatus(
-  //   @Param('id', ParseIntPipe) id: number,
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus
-  // ): Promise<Task> {
-  //   return this.tasksService.updateTaskStatus(id, status)
-  // }
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
+    @GetUser() user: User
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status, user)
+  }
 }
